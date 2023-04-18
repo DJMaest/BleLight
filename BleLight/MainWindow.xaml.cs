14d6af4c36f3379eb3bbe9b8eb0ba08ed9015da2
Alireza Azimi <djmaestroman@gmail.com>
2023-04-17 19:03:39 -0600

    auto detect serial port

--- BleLight/MainWindow.xaml.cs
+++ BleLight/MainWindow.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO.Ports;
-using System.Diagnostics;
 
 namespace BleLight
 {
@@ -23,19 +16,39 @@ namespace BleLight
     public partial class MainWindow : Window
     {
         public String comPort = "";
-        SerialPort mySerialPort;
+        SerialPort? mySerialPort;
         private bool connected = false;
         private const bool debug = false;
         public MainWindow()
         {
             InitializeComponent();
             string[] ports = SerialPort.GetPortNames();
-            foreach (string port in ports)
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
             {
-                availablePorts.Items.Insert(0, port);
+
+                var portVals = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = ports.Select(n => portVals.FirstOrDefault(s => s.Contains(n))).ToList();
+                try
+                {
+                    foreach (string s in portList)
+                    {
+                        if (Regex.Replace(s, "\\s\\(.+\\)", "") == "USB Serial Port")
+                        {
+                            comPort = Regex.Match(s, "\\((.+)\\)").Groups[1].Value;
+                        }
+                    }
+                }
+                catch
+                {
+                    connStatus.Text = "Something went wrong";
+                }
+
             }
-               
+
             connStatus.Text = "Disconnected";
+            availablePort.Text = "Available Port: " + comPort;
         }
         protected override void OnClosed(EventArgs e)
         {
@@ -49,7 +62,8 @@ namespace BleLight
             {
                 mySerialPort.WriteLine("1");
 
-            } else
+            }
+            else
             {
                 mySerialPort.WriteLine("0");
 
@@ -57,22 +71,21 @@ namespace BleLight
 
         }
 
-        private void AvailablePorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private static void ShowWarningDialog(string message, string caption)
         {
-            if(debug)
-                Trace.WriteLine(availablePorts.SelectedItem.ToString());
-           
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBox.Show(message, caption, button, icon, MessageBoxResult.Yes);
         }
 
         private void ConnectSerial_Click(object sender, RoutedEventArgs e)
         {
-   
-            string selectedItem = availablePorts.SelectedItem.ToString();
-            if (!selectedItem.Contains("Choose Serial Port"))
+            try
             {
                 if (mySerialPort == null)
                 {
-                    mySerialPort = new SerialPort(selectedItem)
+
+                    mySerialPort = new SerialPort(comPort)
                     {
                         BaudRate = 9600,
                         Parity = Parity.None,
@@ -82,33 +95,57 @@ namespace BleLight
                         RtsEnable = true
                     };
 
-                    mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-                    if(debug)
-                        Trace.WriteLine("from object: ");
+                    if (!mySerialPort.IsOpen)
+                    {
+                        mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                        if (debug)
+                            Trace.WriteLine("from object: ");
 
-                    mySerialPort.Open();
-
-
+                        mySerialPort?.Open();
+                        mySerialPort?.WriteLine("2");
+                        connectSerial.Content = "Disconnect";
+                    }
+                    else
+                    {
+                        ShowWarningDialog("Serial port already open. Close it first", "Serial Port Issue");
+                        mySerialPort?.Close();
+                    }
+                } else
+                {
+                    mySerialPort?.Close();
+                    mySerialPort = null;
+                    connectSerial.Content = "Serial Connect";
+                    SetConnectionState("Disconnected");
                 }
-                //mySerialPort.Write("2");
-                mySerialPort.WriteLine("2");
 
-            } else
-            {
-                string messageBoxText = "You need to select a port first";
-                string caption = "Can't connect to port";
-                MessageBoxButton button = MessageBoxButton.OK;
-                MessageBoxImage icon = MessageBoxImage.Warning;
-                MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
             }
+            catch (Exception Exception)
+            {
+                ShowWarningDialog("Unable to connect to port.", "Serial Port Issue");
+                mySerialPort?.Close();
+                mySerialPort = null;
+            }
+
         }
 
-        private void PerformLightSwitch(string connectionStatus, string buttonText, string lightBulbStatus)
+
+        private void SetConnectionState(string connectionStatus)
         {
             this.Dispatcher.Invoke(() =>
             {
                 connStatus.Text = connectionStatus;
-                btnSwitch.IsEnabled = true;
+                bool state = connectionStatus == "Connected";
+                btnSwitch.IsEnabled = state;
+                connected = state;
+
+            });
+
+        }
+        private void PerformLightSwitch(string connectionStatus, string buttonText, string lightBulbStatus)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                SetConnectionState("Connected");
                 btnSwitch.Content = buttonText;
                 lightBulbImage.Source = new BitmapImage(new Uri(lightBulbStatus, UriKind.Relative));
             });
@@ -134,31 +171,34 @@ namespace BleLight
                 Trace.WriteLine("Data Received:");
                 Trace.Write(indata);
             }
-
-            if (indata == "1")
+            switch (indata)
             {
-                if (!connected)
-                {
-                    PerformLightSwitch("Connected", "Turn off", "images/lightbulb-on.png");
-                    connected = true;
-                }
-                PerformLightSwitch("Turn off", "images/lightbulb-on.png");
-            } else if (indata == "0")
-            {
-                if (!connected)
-                {
-                    this.Dispatcher.Invoke(() =>
+                case "0":
+                    if (!connected)
                     {
                         PerformLightSwitch("Connected", "Turn on", "images/lightbulb-off.png");
-                    });
-                    connected = true;
-                }
 
-                this.Dispatcher.Invoke(() => {
+                    }
                     PerformLightSwitch("Turn on", "images/lightbulb-off.png");
-                });
+                    break;
+                case "1":
+                    if (!connected)
+                    {
+                        PerformLightSwitch("Connected", "Turn off", "images/lightbulb-on.png");
+
+                    }
+                    PerformLightSwitch("Turn off", "images/lightbulb-on.png");
+                    break;
+                case "+CONNECT":
+                    SetConnectionState("Connected");
+                    break;
+                case "+DISCONNECT":
+                    SetConnectionState("Disconnected");
+                    break;
             }
+
 
         }
     }
 }
+
